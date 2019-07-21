47d21e192cf28e8c7867af68ca0cde2886f1af7e
Alexander Zautke <mail@alexander-zautke.com>
2019-07-21 21:38:40 +0200

    Configure middleware in separate configuration

--- Vonk.Plugin.ExampleOperation/VonkPluginConfiguration.cs
+++ Vonk.Plugin.ExampleOperation/VonkPluginConfiguration.cs
@@ -9,7 +9,7 @@ using Vonk.Core.Support;
 namespace Vonk.Plugin.ExampleOperation
 {
     [VonkConfiguration(order: 4600)] // Check for potential conflicts!
-    public class VonkPluginConfiguration
+    public class VonkExampleServiceConfiguration
     {
         /* Add services here to the DI system of ASP.NET Core
            Make sure to register services that you depend upon as well. 
@@ -25,11 +25,25 @@ namespace Vonk.Plugin.ExampleOperation
         // Add middleware to the pipeline being built with the builder
         public static IApplicationBuilder Configure(IApplicationBuilder builder)
         {
-            // Register Middleware, can also be registered independantly of the VonkPluginService using a seperate configuration class
-            builder.UseMiddleware<VonkPluginMiddleware>();
             // Register interactions (Don't add a "$" sign to the name of the custom operation, it will be added by default)
             builder.OnCustomInteraction(VonkInteraction.instance_custom, "test").AndMethod("GET").HandleAsyncWith<VonkPluginService>((svc, context) => svc.Test(context));
             builder.OnCustomInteraction(VonkInteraction.type_custom, "test").AndMethod("POST").HandleAsyncWith<VonkPluginService>((svc, context) => svc.Test(context));
+            return builder;
+        }
+    }
+
+    [VonkConfiguration(order: 4590)]
+    public class VonkExampleMiddlewareConfiguration
+    {
+        public static IServiceCollection ConfigureServices(IServiceCollection services)
+        {
+            // VonkPluginMiddleware doesn't have any dependencies on other services, otherwise they should be registered here
+            return services;
+        }
+
+        public static IApplicationBuilder Configure(IApplicationBuilder builder)
+        {
+            builder.UseMiddleware<VonkPluginMiddleware>();
             return builder;
         }
     }
