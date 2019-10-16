4344134e49fbe10a9c69f00f780322667dbe8abe
Alexander Zautke <mail@alexander-zautke.com>
2019-10-16 09:45:16 +0200

    Make VonkPluginMiddleware internal explicitly

--- Vonk.Plugin.ExampleOperation/VonkPluginMiddleware.cs
+++ Vonk.Plugin.ExampleOperation/VonkPluginMiddleware.cs
@@ -7,7 +7,7 @@ using Vonk.Core.Support;
 
 namespace Vonk.Plugin.ExampleOperation
 {
-    class VonkPluginMiddleware
+    internal class VonkPluginMiddleware
     {
         private readonly RequestDelegate _next;
         private readonly ILogger<VonkPluginService> _logger;
