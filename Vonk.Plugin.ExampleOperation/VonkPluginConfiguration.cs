99686512d54bd8f743a77b3a10155cc1ca80db8e
Alexander Zautke <mail@alexander-zautke.com>
2019-10-20 08:04:22 +0200

    Fix comment

--- Vonk.Plugin.ExampleOperation/VonkPluginConfiguration.cs
+++ Vonk.Plugin.ExampleOperation/VonkPluginConfiguration.cs
@@ -19,7 +19,7 @@ namespace Vonk.Plugin.ExampleOperation
         public static IServiceCollection ConfigureServices(IServiceCollection services)
         {
             services.TryAddScoped<VonkPluginService>(); // Add the service implementation
-            services.TryAddContextAware<ICapabilityStatementContributor, VonkPluginCapabilityStatementContributor>(ServiceLifetime.Transient); // Add operation to Vonk's CapabilityStatement (Only for STU-3)
+            services.TryAddContextAware<ICapabilityStatementContributor, VonkPluginCapabilityStatementContributor>(ServiceLifetime.Transient); // Add operation to Vonk's CapabilityStatement
             return services;
         }
 
